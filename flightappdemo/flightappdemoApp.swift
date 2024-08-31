04c105bbadccc0fa9b73672e5a220d802eb45a6e
mustafath <mustafadalkesen11@gmail.com>
2024-08-31 23:23:55 +0300

    Initial Commit

---
+++ flightappdemo/flightappdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  flightappdemoApp.swift
+//  flightappdemo
+//
+//  Created by Mustafa Girgin on 31.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct flightappdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
